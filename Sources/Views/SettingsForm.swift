9aee0e3877688dce7effa5f37424821487c1bff1
Reed Es <reedes@fastmail.com>
2023-01-13 23:35:24 -0700

    ongoing

--- Sources/Views/SettingsForm.swift
+++ Sources/Views/SettingsForm.swift
@@ -53,9 +53,9 @@ public struct SettingsForm<Bottom>: View
                     .foregroundStyle(.tint)
             } footer: {
                 #if os(watchOS)
-                    Text("Recent history will be stored locally for up to 1 year. Periodically run iOS app for long-term storage and review.")
+                    Text("Recent history will be stored locally for up to 1 year. Periodically run iPhone/iPad app for long-term storage and review.")
                 #elseif os(iOS)
-                    Text("History can be reviewed from a tab on the home screen.")
+                    Text("History can be reviewed from the home screen.")
                 #endif
             }
 
