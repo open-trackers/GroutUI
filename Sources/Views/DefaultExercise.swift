a653f9dd20360aa2d808eca8111ee3915fb328ea
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-02-05 08:26:37 +0200

    - Removed the VStack. - Explicit font styles are changed to defualt styles. - Moved actions into separate functions.

--- Sources/Views/DefaultExercise.swift
+++ Sources/Views/DefaultExercise.swift
@@ -25,51 +25,44 @@ struct DefaultExercise: View {
     @State var defaultUnit: Units = (Units(rawValue: Int16(DefaultUnitStored.DefaultUnit.defaultUnitStored)) ?? .kilograms)
     
     private init() {}
-    
+
     var body: some View {
-        VStack {
+
             //MARK: Sets
             Section {
                 Stepper(value: $defaultSet, in: 0 ... 10, step: 1) {
                     Text("\(defaultSet)")
-                        .font(.custom("Arial", size: 27))
+                        .font(.title) // for consistency (use same intensity step font)
                 } onEditingChanged: { _ in
                     Haptics.play()
                 }
-                //            .tint(tint)
             } header: {
                 Text("Set Count")
-                //                .foregroundStyle(tint)
             }
             
             //MARK: Repetitions
             Section {
                 Stepper(value: $defaultRep, in: 0 ... 100, step: 1) {
                     Text("\(defaultRep)")
-                        .font(.custom("Arial", size: 27))
+                        .font(.title) // for consistency (use same intensity step font)
                 } onEditingChanged: { _ in
                     Haptics.play()
                 }
-                //            .tint(tint)
             } header: {
                 Text("Repetition Count")
-                //                .foregroundStyle(tint)
             }
             
             //MARK: Intensity Step
             Section {
                 Stepper(value: $defaultIntensity, in: 0.1 ... 25.0, step: 0.1) {
                     Text(String(format: "%.1f", defaultIntensity))
-                        .font(.custom("Arial", size: 27))
+                        .font(.title) // to avoid decimal viewed on two lines
                 } onEditingChanged: { _ in
                     Haptics.play()
                 }
-                //                    .tint(tint)
             } header: {
                 Text("Intensity Step")
-                //                 .foregroundStyle(tint)
             }
-            Spacer()
             //MARK: Units
             Section {
                 Picker("Choose Unit", selection: $defaultUnit) {
@@ -79,36 +72,34 @@ struct DefaultExercise: View {
                 }
                 .onChange(of: defaultUnit) { _ in
                     Haptics.play()
-                    DefaultUnitStored.DefaultUnit.defaultUnitStored = Int(defaultUnit.rawValue)
-                    print("Current Unit Stored is: \(Units(rawValue: Int16(DefaultUnitStored.DefaultUnit.defaultUnitStored))?.formattedDescription ?? "No Unit Selected")")
+                    storeUnit()
                 }
                 .pickerStyle(.wheel)
-                .frame(height: 50)
+                .frame(height: 50) //the default height is too small
             }
-//
-            
             //MARK: Reset
                 Button(action: {
                     Haptics.play()
-                    defaultSet = 3
-                    defaultRep = 12
-                    defaultIntensity = 10.0
-                    DefaultUnitStored.DefaultUnit.defaultUnitStored = 2
-                    defaultUnit = .kilograms
+                    resetSettings()
                 }) {
-                    Text("Reset")
-//                        .foregroundStyle(tint)
+                    HStack { Spacer(); Text("Reset"); Spacer(); } // center button title
                 }
-                .frame(width: 50)
-                .frame(height: 30)
-                .padding()
                 .foregroundColor(.orange)
-                .buttonStyle(PlainButtonStyle()) //Button triggered by UI seems to be an issue with SwiftUI generally. using .buttonStyle(PlainButtonStyle()) will fix the problem. https://stackoverflow.com/questions/68541909/two-buttons-in-swift-ui-on-watchos
-        }
+                .font(.title2) // Highlight the button
+                .buttonStyle(PlainButtonStyle()) // prevent UI from triggering the button
+    }
+    
+    //MARK: Actions
+    private func resetSettings() {
+        defaultSet = 3
+        defaultRep = 12
+        defaultIntensity = 10.0
+        DefaultUnitStored.DefaultUnit.defaultUnitStored = 2
+        defaultUnit = .kilograms
+    }
+    private func storeUnit() {
+        DefaultUnitStored.DefaultUnit.defaultUnitStored = Int(defaultUnit.rawValue)
+        print("Current Unit Stored is: \(Units(rawValue: Int16(DefaultUnitStored.DefaultUnit.defaultUnitStored))?.formattedDescription ?? "No Unit Selected")")
     }
 }
 
-/*I have the same issue, didn't matter how I modified the Picker, put it inside List, ScrollView, Form, etc. Same warning. Seems to me as you mention it is a bug on SwiftUI for watchOS, but I would think is harmless to ignore the warning (I hope...) –
- vicegax
- Oct 12, 2022 at 17:16 - StackoverFlow
- */
